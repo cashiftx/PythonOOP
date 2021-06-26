38beda12273eb618f88785601d734e3ee962f8e7
Cashif <cashiftx@gmail.com>
2021-06-27 00:01:05 +0500

    Intial Commit

---
+++ TestObjects.py
@@ -0,0 +1,9 @@
+class Animals:
+    def __init__(self,name,color,age):
+        self.name =name
+        self.color = color
+        self.age = age
+    #    self.name = name
+
+Lion = Animals("Leo","Orange",7)
+print("Lion's name is " + Lion.name + " and Lion's age is " + str(Lion.age))
