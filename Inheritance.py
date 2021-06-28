58dd8b975ae3a2cc500ac48095087b3d624e28e3
Cashif <cashiftx@gmail.com>
2021-06-28 23:59:27 +0500

    Inheritance (Parent/Child Classes) with #Comments + Fixed None Value parent Print Details fn

--- Inheritance.py
+++ Inheritance.py
@@ -1,17 +1,24 @@
-class parent: #Main CLass
-    def __init__(self,name,fame):
-        self.name=name
-        self.fame =fame
+class parent:  # Main CLass
+    def __init__(self, name, fame):
+        self.name = name
+        self.fame = fame
 
-Github = parent("Github","99%")
-print("Parent name is " + Github.name)
+    def printDetails(self):
+        print("Parent name is " + Github.name + "Parent fame is " + Github.fame)
 
-class child(parent): #Main CLass
-    def __init__(self,name,fame,car,bike): #merge all parameters parent + child
-        super().__init__(name,fame)         #Args of Parent class only
-        self.car = car                      #Initialize self object
+Github = parent("Github", "99%")
+#print("Parent name is " + Github.name)
+
+
+
+Github.printDetails()
+class child(parent):  # Main CLass
+    def __init__(self, name, fame, car, bike):  # merge all parameters parent + child
+        super().__init__(name, fame)  # Args of Parent class only
+        self.car = car  # Initialize self object
         self.bike = bike
-git = child("Git","90","Many","None")
-#calling
-print("Child Name: "  + git.name)
-print("Child Fame: "  + git.fame)
+
+git = child("Git", "90", "Many", "None")
+# calling
+print("Child Name: " + git.name)
+print("Child Fame: " + git.fame)
